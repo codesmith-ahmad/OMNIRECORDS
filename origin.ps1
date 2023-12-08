0a60d2c719284f718ddc54038a865db39d6c64d5
CavalierAhmad <alja0062@algonquinlive.com>
2023-12-08 08:33:00 -0500

    modified config and origin

--- origin.ps1
+++ origin.ps1
@@ -4,7 +4,7 @@ $origin = "C:\OMNIRECORDS"  # Define origin
 Set-Location $origin
 
 # Load config settings
-$configFilePath = "$origin\config.json"                # Path of config.json file
+$configFilePath = ".\config.json"                      # Path of config.json file
 $jsonContent = Get-Content -Path $configFilePath -Raw  # Read the content of the JSON file
 $config = $jsonContent | ConvertFrom-Json -AsHashtable # Can access properties like $config.PropertyKey
 
@@ -13,11 +13,6 @@ $Host.UI.RawUI.WindowTitle = $config.consoleSettings.windowTitle
 $Host.UI.RawUI.BackgroundColor = $config.consoleSettings.backgroundColor
 $Host.UI.RawUI.ForegroundColor = $config.consoleSettings.foregroundColor
 
-# Import modules
-. "$origin\Scripts\importModules"
-
-# Import functions
-. "$origin\Scripts\importFunctions.ps1"
-
-# Welcome user
-. .\Scripts\welcome.ps1
+. ".\Scripts\importModules"       # Import modules
+. ".\Scripts\importFunctions.ps1" # Import functions
+. ".\Scripts\welcome.ps1"         # Welcome user
