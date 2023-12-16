84f0c7cda2c8099f010f5de91eea85d307a9cc5e
codesmith-ahmad <alias.000@outlook.com>
2023-12-16 09:48:17 -0500

    TODO: 1. Create and import other tables 2. continue 'update.ps1' to update Expenses

--- Functions/update.ps1
+++ Functions/update.ps1
@@ -1,4 +1,17 @@
 function update ($id){
     if (-not $id){Write-Host "SPECIFY ID"}
-    
+    $id = $id.ToLower()
+    if ($id[0] -eq 'x'){echo "ITS X"}
+    else {
+        Write-Host "NO TABLE ASSOCIATED WITH" $id[0]
+        Write-Host @"
+        A: Assignments
+        C: Credentials
+        D: Documents
+        E: Events
+        L: Loans
+        T: Tasks
+        X: Expenses
+"@
+    }
 }
