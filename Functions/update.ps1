3289416a837f55213bd17349bfacc4e8d0ae7303
codesmith-ahmad <alias.000@outlook.com>
2023-12-20 03:02:28 -0500

    Now function 'update' correctly parses id and routes to appropriate subfunction, see update.ps1. Last TODO remains

--- Functions/update.ps1
+++ Functions/update.ps1
@@ -1,17 +1,56 @@
-function update ($id){
-    if (-not $id){Write-Host "SPECIFY ID"}
-    $id = $id.ToLower()
-    if ($id[0] -eq 'x'){echo "ITS X"}
+# Routing function
+function update ($idString){
+    if (-not $idString){Write-Host "`e[31mSPECIFY ID`e[0m"}
     else {
-        Write-Host "NO TABLE ASSOCIATED WITH" $id[0]
-        Write-Host @"
-        A: Assignments
-        C: Credentials
-        D: Documents
-        E: Events
-        L: Loans
-        T: Tasks
-        X: Expenses
+        $parsedID = idParser $idString
+        if ($parsedID[0] -eq 0){echo "break!" ; break}
+        $table = $parsedID[0].ToLower()
+        $id = $parsedID[1]
+        if ($table -eq 'a'){updateAssignments $id}
+        elseif ($table -eq 'c'){updateCredentials $id}
+        elseif ($table -eq 'd'){updateDocuments $id}
+        elseif ($table -eq 'e'){updateEvents $id}
+        elseif ($table -eq 'l'){updateLoans $id}
+        elseif ($table -eq 't'){updateTasks $id}
+        elseif ($table -eq 'x'){updateExpenses $id}
+        else {
+            Write-Host "`e[31mNO TABLE ASSOCIATED WITH`e[0m `""$idString[0]"`""
+            Write-Host @"
+            `e[92mA: Assignments
+            C: Credentials
+            D: Documents
+            E: Events
+            L: Loans
+            T: Tasks
+            X: Expenses`e[0m
 "@
+        }
     }
+}
+
+function updateAssignments ($id) {
+    Write-Host (fgo "************************TODO ASSIGNMNETS $id*********************")
+}
+
+function updateCredentials ($id) {
+    Write-Host (fgo "************************TODO CREDENTIALS $id*********************")
+}
+
+function updateDocuments ($id) {
+    Write-Host (fgo "************************TODO DOCS $id*********************")
+}
+
+function updateEvents ($id) {
+    Write-Host (fgo "************************TODO EVENTS $id*********************")
+}
+
+function updateLoans ($id) {
+    Write-Host (fgo "************************TODO LOANS $id*********************")
+}
+
+function updateTasks ($id) {
+    Write-Host (fgo "************************TODO TASKS $id*********************")
+}
+function updateExpenses ($id){
+    Write-Host (fgo "************************TODO EXPENSES $id*********************")
 }
