325fab70f6df006d849d9c79feba201a0a74131f
codesmith-ahmad <alias.000@outlook.com>
2023-12-20 03:02:28 -0500

    Now function 'update' correctly parses id and routes to appropriate subfunction, see update.ps1. Last TODO remains

---
+++ Functions/idParser.ps1
@@ -0,0 +1,25 @@
+# TESTED
+
+# Parses the id string for update.ps1
+# And returns an array of [letters,number]
+
+function idParser {
+    param (
+        [string]$inputString
+    )
+
+    # Use a regular expression to match the letters and numbers
+    $match = $inputString -match '^([a-zA-Z]{1,3})(\d{1,3})$'
+
+    if ($match) {
+        $letters = $matches[1]
+        $number = [int]$matches[2]
+
+        # Output the parsed values
+        return ($letters,$number)
+    }
+    else {
+        Write-Host "`e[7m`e[31mInvalid format`e[0m"
+        return (0,'error')
+    }
+}
