5d15fa12606d43c5d741be3f6af8c68b337c4591
codesmith-ahmad <alias.000@outlook.com>
2024-01-11 15:57:49 -0500

    successfully created a timeslots table which contains ALL increments of 30 minutes beginning from Sunday 0000 to Saturday 2330. Use it for schedule table

---
+++ Functions/timeslots.ps1
@@ -0,0 +1,31 @@
+function create-timeslots {
+    $c = 1
+    $w = 0
+    $h = 0
+    $m = 0
+
+    while ($w -lt 7){
+
+        $timeslotId = $w*1000 + $h*10 + $m*5
+        $startTime = date ("$h" + ":" + $m * 30)
+        $endTime   = $startTime.AddMinutes(30)
+        $startString = $startTime.ToString("HH:mm")
+        $endString   = $endTime.ToString("HH:mm")
+
+        write-host "($timeslotId, $w, `"$startString`", `"$endString`"),"
+
+        $m++
+        if ($m -eq 2){
+            $h++
+            $m = 0
+            if ($h -eq 24){
+                $w++
+                $h = 0
+            }
+        }
+
+    }
+
+}
+
+create-timeslots
