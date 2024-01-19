1632871f47c9707e8066e48391e256b82d379a9f
codesmith-ahmad <alias.000@outlook.com>
2024-01-19 09:28:17 -0500

    tidying up git folder
    Former-commit-id: afb12e319af1d9a931f52b5bb6ac20bd43d8247d

--- Functions/archive.ps1
+++ Functions/archive.ps1
@@ -1,20 +1,20 @@
-function archive ($path) {
-    if (-not $path){Write-Host "`e[31mSPECIFY FILEPATH TO ARCHIVE`e[0m"}
-    else {
-        $nameOfFile = Read-Host "Name of file"
-        Write-Host "path is" $path
-        Write-Host "name of file is" $nameOfFile
+# function archive ($path) {
+#     if (-not $path){Write-Host "`e[31mSPECIFY FILEPATH TO ARCHIVE`e[0m"}
+#     else {
+#         $nameOfFile = Read-Host "Name of file"
+#         Write-Host "path is" $path
+#         Write-Host "name of file is" $nameOfFile
 
-        $newDirectory = "C:\OMNIRECORDS\OmniRecords-Database\OmniRecords-Library\Archives\" + $nameOfFile
-        New-Item  -ItemType Directory -Path $newDirectory
-        Copy-Item -Path $path  -Destination $newDirectory
+#         $newDirectory = "C:\OMNIRECORDS\OmniRecords-Database\OmniRecords-Library\Archives\" + $nameOfFile
+#         New-Item  -ItemType Directory -Path $newDirectory
+#         Copy-Item -Path $path  -Destination $newDirectory
         
-        $query = "INSERT INTO Archives (Name) VALUES ('" + $nameOfFile + "')"
-        sql $query
-        sql 'SELECT * FROM Archives' | Format-Table
+#         $query = "INSERT INTO Archives (Name) VALUES ('" + $nameOfFile + "')"
+#         sql $query
+#         sql 'SELECT * FROM Archives' | Format-Table
 
-        Write-Host "File saved at:"
-        $output = "" + $config.database + "\..\OmniRecords-Library\Archives\$nameOfFile"
-        Write-Host $output
-    }
-}
+#         Write-Host "File saved at:"
+#         $output = "" + $config.database + "\..\OmniRecords-Library\Archives\$nameOfFile"
+#         Write-Host $output
+#     }
+# }
