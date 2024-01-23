86a9d319f870bc5b2143d2438deb010a4ae5fda1
codesmith-ahmad <alias.000@outlook.com>
2024-01-23 14:35:23 -0500

    update db

--- Functions/check.ps1
+++ Functions/check.ps1
@@ -5,7 +5,10 @@ function check {
 
     if ($args.Count -gt 1){Write-Host $red"TOO MANY ARGS"$0d; break}
 
-    if     ($process -match "^*mail*" -or $process -match "\b(tt|timetable)\b"){write-host "Email!"}
+    if     ($process -match "^*mail*" -or $process -match "\b(tt|timetable)\b"){
+        . "C:\Program Files\WindowsApps\Microsoft.OutlookForWindows_1.2024.103.100_x64__8wekyb3d8bbwe\olk.exe"
+    }
     elseif ($process -match "(todo|^*task*)"){write-host "${yellow}OPEN ${line}MICROSOFT TO DO$0d`n"}
+    elseif ($process -match "regex")         {}
     else {write-host "`n${red}NO SUCH PROCESS`e[0m`n"}
 }
