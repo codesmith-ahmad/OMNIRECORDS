a3b0b78fbcfbba1408694eefcf70b4f4f33e45d6
codesmith-ahmad <alias.000@outlook.com>
2024-01-24 00:23:04 -0500

    modified origin.ps1. Goal is to update db current time and modify a checklist using that

---
+++ Scripts/UISettings.ps1
@@ -0,0 +1,4 @@
+# Set UI settings
+$Host.UI.RawUI.WindowTitle = $config.consoleSettings.windowTitle
+$Host.UI.RawUI.BackgroundColor = $config.consoleSettings.backgroundColor
+$Host.UI.RawUI.ForegroundColor = $config.consoleSettings.foregroundColor
