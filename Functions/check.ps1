3bd219613230c252d84e34881fc0a83e81c51ffb
codesmith-ahmad <alias.000@outlook.com>
2024-01-24 12:31:02 -0500

    fixed check.ps1 (windowsapp link changed overnight)
    Former-commit-id: 700c815b2a6c942200f7b008af743898ffc9237f

--- Functions/check.ps1
+++ Functions/check.ps1
@@ -6,9 +6,13 @@ function check {
     if ($args.Count -gt 1){Write-Host $red"TOO MANY ARGS"$0d; break}
 
     if     ($process -match "^*mail*" -or $process -match "\b(tt|timetable)\b"){
-        . "C:\Program Files\WindowsApps\Microsoft.OutlookForWindows_1.2024.103.100_x64__8wekyb3d8bbwe\olk.exe"
+        . "C:\OMNIRECORDS\olk.exe.lnk"
     }
-    elseif ($process -match "(todo|^*task*)"){write-host "${yellow}OPEN ${line}MICROSOFT TO DO$0d`n"}
-    elseif ($process -match "regex")         {}
+    elseif ($process -match "(todo|^*task*)"){
+        write-host "${yellow}OPEN ${line}MICROSOFT TO DO$0d`n"
+    }
+    elseif ($process -match "regex"){
+        # template
+    }
     else {write-host "`n${red}NO SUCH PROCESS`e[0m`n"}
 }
--- Scripts/updateDB.ps1
+++ Scripts/updateDB.ps1
@@ -1,2 +1,5 @@
+# To make sure I don't forget to pull before I sync
+# To automate tasks using SQLite by updating datetime when launching Powershell
+
 git pull
 Write-Host ""
