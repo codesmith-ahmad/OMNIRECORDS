eefa3c7a6baa900ec19085e69582d57e68080b45
codesmith-ahmad <alias.000@outlook.com>
2024-01-25 11:31:56 -0500

    2024-Jan-25 T1131

--- Scripts/updateDB.ps1
+++ Scripts/updateDB.ps1
@@ -1,12 +1,14 @@
 # To make sure I don't forget to pull before I sync
 # To automate tasks using SQLite by updating datetime when launching Powershell
 
-git pull
-Write-Host ""
+# git pull
+# Write-Host ""
 
-sql @"
-UPDATE _timestamp SET
-    unix = (SELECT strftime('%s', 'now')),
-    julian = (SELECT julianday('now')),
-    iso8601 = (SELECT datetime('now'))
-"@
+# sql @"
+# UPDATE _timestamp SET
+#     unix = (SELECT strftime('%s', 'now')),
+#     julian = (SELECT julianday('now')),
+#     iso8601 = (SELECT datetime('now'))
+# "@
+
+# USELESS
