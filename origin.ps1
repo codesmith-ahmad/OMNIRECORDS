b8afd9f609cd87130be30c979d96979d0344fd4e
codesmith-ahmad <alias.000@outlook.com>
2024-02-16 17:32:19 -0500

    OMNIRECORDS SCRIPT INTERFERING WITH PYTHON SCRIPT, HAD TO MOVE IT
    Former-commit-id: 77ccc69785b907dadc4a6c560e2d41c9b2564a20

--- origin.ps1
+++ origin.ps1
@@ -1,16 +1,13 @@
 
-# Set the origin point of this workspace
-$origin = "C:\OMNIRECORDS"  # Define origin
-Set-Location $origin
+function launch {
+    param (
+        [string]$inputString
+    )
 
-# MODIFICATION OF PATH VARIABLES (LIST OF DIRECTORIES)
-$env:Path += ';C:\sqlite'
-
-# SCRIPTS
-
-. ".\Scripts\loadConfigJSON"      # Load config settings from JSON
-. ".\Scripts\UISettings"          # Set UI settings
-. ".\Scripts\importModules"       # Import modules
-. ".\Scripts\importFunctions.ps1" # Import functions
-. ".\Scripts\welcome.ps1"         # Welcome user
-. ".\Scripts\updateDB"            # Update OMNIRECORDS
+    if ($inputString -eq "omnirecords") {
+        . ".\launchOmnirecords.ps1"
+    }
+    else {
+        write-host "Omnirecords is the only option for now"
+    }
+}
